a4d9927de873bf47d04e806e5bd33abe3efb1623
andrioli22 <134887295+andrioli22@users.noreply.github.com>
2024-01-15 12:08:08 -0300

    Create Desconto.js

---
+++ Desconto.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 3 JavaScript</title>
+    <script>
+        //Peça ao usuário o valor do preço normal do produto
+        const Normal=prompt("Digite o preço normal do produto ");
+
+        //preço com desconto de 10%
+        const descNormal=Normal*0.9;
+
+        //exibir o preço com desconto
+        alert("o valor com 10% de desconto é "+descNormal)
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
