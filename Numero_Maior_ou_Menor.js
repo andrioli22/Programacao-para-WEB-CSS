69747c8865cc50cdbbec15c0a66d71c6ed949c88
andrioli22 <134887295+andrioli22@users.noreply.github.com>
2024-01-15 12:10:19 -0300

    Create Numero_Maior_ou_Menor.js

---
+++ Numero_Maior_ou_Menor.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 6</title>
+    <script>
+         //Leia o primeiro número
+        const N1=prompt("Digite o primeiro número");
+
+        //Leia o segundo número
+        const N2=prompt("Digite o segundo número");
+
+        //Leia o trerceiro número
+        const N3=prompt("Digite o terceiro número");
+
+        //Dar valor ao número
+        NUMERO1=parseInt(N1);
+        NUMERO2=parseInt(N2);
+        NUMERO3=parseInt(N3);
+
+        //Mostrar qual é o maior e qual é o menor
+        alert("O número maior é "+Math.max(NUMERO1, NUMERO2), NUMERO3);
+
+        alert("O número menor é "+Math.min(NUMERO1, NUMERO2));
+
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
