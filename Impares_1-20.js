b24f64e727903ce2cb23801338ca9d5903ab0f65
andrioli22 <134887295+andrioli22@users.noreply.github.com>
2024-01-15 12:12:30 -0300

    Create Impares_1-20.js

---
+++ Impares_1-20.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio 10 JavaScript</title>
+    <script>
+        for (let index=1;index<=20;index++){
+            if(index%2==1){
+                document.write(index+"<br>");
+            }
+            
+        }
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
