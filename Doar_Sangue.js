d797c2fcb216208bbb312e1a3fb1345105fd1e7b
andrioli22 <134887295+andrioli22@users.noreply.github.com>
2024-01-15 12:11:33 -0300

    Create Doar_Sangue.js

---
+++ Doar_Sangue.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 8 Java Script</title>
+    <script>
+        //Leia a idade da pessoa 
+        const N1=Number(prompt("Digite a sua idade"));
+
+        //Identificar se ela pode doar sangue ou não
+        if(N1>=18 && N1<=67){
+            alert("Pode doar sangue");
+        }
+        else {
+            alert("Não pode doar sangue");
+        }
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
