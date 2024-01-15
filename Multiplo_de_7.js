6135bb4144ec845cd2da2ae1b934fa4c25a459da
andrioli22 <134887295+andrioli22@users.noreply.github.com>
2024-01-15 12:10:50 -0300

    Create Multiplo_de_7.js

---
+++ Multiplo_de_7.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 7 Java Script</title>
+    <script>
+        //Leia o número inteiro
+        const N1=Number(prompt("Digite o primeiro número"));
+
+        //Mostre na tela se é múltiplo de 7 ou não
+        if (N1%7==0){
+        alert("o número é divisível por 7");
+        }
+        else {
+        alert("o número não é divisível por 7");
+        }
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
